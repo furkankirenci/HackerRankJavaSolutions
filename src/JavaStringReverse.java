1fc556b1df96e56b24f8f5695fc4537865a4005c
furkankirenci <kirencifurkan1@gmail.com>
2022-07-08 20:50:39 +0300

    Java String Reverse

---
+++ src/JavaStringReverse.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+public class JavaStringReverse {
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        String reverse = "";
+        for(int i = A.length()-1 ; i>= 0 ; i--){
+            reverse += A.charAt(i);
+        }
+        if(reverse.equals(A)){
+            System.out.println("Yes");
+        }else{
+            System.out.println("No");
+        }
+    }
+}
